b82368e69530df71ff4d75e07451c54d914cc664
Saloni Sharma <142489932+Salonisharmaaa@users.noreply.github.com>
2024-07-15 22:01:17 +0530

    Update BFS Traversal in graph.cpp

--- Day1/BFS Traversal in graph.cpp
+++ Day1/BFS Traversal in graph.cpp
@@ -5,26 +5,23 @@ class Solution {
         vector<int>vis(V,0);
         queue<int>q;
         vector<int>ans;
+            
+        q.push(0);
+        vis[0] = 1;
         
-        for(int i =0;i<V;i++){
-            if(vis[i] == 0){
-                q.push(i);
-                vis[i] = 1;
-                
-                while(!q.empty()){
-                    int top = q.front();
-                    ans.push_back(top);
-                    q.pop();
-                    
-                    for(auto a:adj[top]){
-                        if(!vis[a]){
-                            q.push(a);
-                            vis[a] = 1;
-                        }
-                    }
+        while(!q.empty()){
+            int top = q.front();
+            ans.push_back(top);
+            q.pop();
+            
+            for(auto a:adj[top]){
+                if(!vis[a]){
+                    q.push(a);
+                    vis[a] = 1;
                 }
             }
         }
+          
         return ans;
     }
 };
